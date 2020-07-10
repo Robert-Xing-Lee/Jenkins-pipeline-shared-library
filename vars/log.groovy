bef8cfd077fef78bf7eff0cd2a991ab85b2d7a70
Robert Lee <robertxinglee@gmail.com>
2020-07-11 02:01:00 +0800

    init

---
+++ vars/log.groovy
@@ -0,0 +1,15 @@
+def info(message) {
+    echo "INFO: ${message}"
+}
+
+def warning(message) {
+    echo "WARNING: ${message}"
+}
+
+def error(message) {
+    echo "ERROR: ${message}"
+}
+
+def debug(message) {
+    echo "DEBUG: ${message}"
+}
